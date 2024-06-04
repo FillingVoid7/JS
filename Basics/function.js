d73f1abca0a9f13b7205b38ccef74d2028d00a62
Corex_7 <130200590+FillingVoid7@users.noreply.github.com>
2024-06-04 08:10:16 +0000

    good

---
+++ Basics/function.js
@@ -0,0 +1,12 @@
+//function declaration
+function add(num1 ,num2){
+    return num1+num2;
+}
+console.log(add(4,5));
+
+//function expression
+let sub = function(num3,num4){
+    return num3-num4;
+}
+let result = sub(2,3);
+console.log(result);
